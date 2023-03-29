b993b95e4012b2d6fcfe255b8c0bcbba1295f27d
Piquenet <clement.piquenet@gmail.com>
2023-03-29 14:42:33 +0200

    commit before cd

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,22 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 export default function Navigation() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/Challenges">Challenges</Link>
-      <Link to="/CreateChallenge">CreateChallenge</Link>
-      <Link to="/Profile">Profile</Link>
+    <div className="navigation-container">
+      <Link to="/" className="navigation-link">
+        Home
+      </Link>
+      <Link to="/Challenges" className="navigation-link">
+        Challenges
+      </Link>
+      <Link to="/CreateChallenge" className="navigation-link">
+        Create Challenge
+      </Link>
+      <Link to="/Profile" className="navigation-link">
+        Profile
+      </Link>
     </div>
   );
 }
