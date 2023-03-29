b03ab0b6d6cb2a232efab800fe2beda327d98d64
nicovray <auvnic@gmail.com>
2023-03-29 11:05:37 +0200

    essai github action

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,17 +25,10 @@ export default function Home() {
   return (
     <div>
       <Navigation />
-      <a
-          className="test"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Home Page
-        </a>
+      <p>Home Page</p>
       <h1>Bienvenue sur "Eco-Challenges" !</h1>
       <p>
-        Envie de lancer un challenge à vos amis ? "Nom de l'App" vous permet de
+        Envie de lancer un challenge à vos amis ? "Eco-Challenge" vous permet de
         créer des challenges personnalisés constitués d'éco-gestes à réaliser
         pour vous informer et sensibiliser sur votre impact énergétique au
         quotidien !
